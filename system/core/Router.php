dfbde61c0dee38d20c9582aa35bee9e40c0352cb
Olga Potapenko <kolholga.yahoo.com>
2021-01-14 21:21:08 +0300

    create project tree

---
+++ system/core/Router.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class Router
+{
+    public static $routers = []; //массив с маршрутами (таблица маршрутов)
+    public $rout; //текущий маршрут
+
+    public function __construct()
+    {
+        //echo 'Router';
+    }
+
+    public static function getRouts($pattern, $path) //правило на вход получает
+    {
+        self::$routers[$pattern] = $path;
+        self::dispatch($path);
+    }
+
+    public static function dispatch($path) //на вход принимет из адресной строки, н-р news/index
+    {
+        foreach (self::$routers as $pattern => $item){
+            preg_match("#".$pattern."#", $item, $matches);
+            pr($matches);
+        }
+    }
+}
